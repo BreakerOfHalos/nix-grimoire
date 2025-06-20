447a134c34edd9eeab1bbbb51fe37a742b7b4c8a
The Heretic's Court <me@breakerofhalos.monster>
2025-06-19 22:25:36 -0700

    Updated wayland and xserver settings.

--- modules/nixos/system/xserver.nix
+++ modules/nixos/system/xserver.nix
@@ -5,10 +5,10 @@
   ...
 }:
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mergeAttrsList optionalAttrs;
 in
 {
-  config = mkIf config.grimoire.profiles.graphical.enable {
+  config = mkIf (config.grimoire.profiles.graphical.enable && !config.grimoire.profiles.gaming.enable) {
     services.xserver = {
       enable = false;
       desktopManager.xterm.enable = false;
